a4003c30c6bfa2bb5f033ca16299b298f246f1a6
GuillemotClement <guillemot.clement@protonmail.com>
2026-01-11 23:03:31 +0100

    FEAT: add login page

--- front/src/pages/Auth/Login.svelte
+++ front/src/pages/Auth/Login.svelte
@@ -1,71 +1,143 @@
 <script lang="ts">
-  // // on utilise un état pour le formulaire
-  // let formState = $state<Omit<Task, 'id'>>({
-  //   title: '',
-  //   isDone: false,
-  // })
+  import { z } from "zod";
+    import { authClient } from "../../libs/auth-client";
+    import { goto } from "@mateothegreat/svelte5-router";
 
-  // let isSubmitting = $state(false);
+  const schema = z.object({
+    email: z.email(),
+    password: z.string()
+  });
 
-  // async function handleSubmit(e: Event){
-  //   // empeche le rechargement de la page
-  //   e.preventDefault();
+  type LoginFormProps = {
+    email: string;
+    password: string;
+  }
 
-  //   isSubmitting = true;
+  let formState = $state<LoginFormProps>({
+    email: "",
+    password: ""
+  });
 
-  //   // on fait la requête
-  //   try{
-  //     const res = await fetch('http://localhost:3000/tasks', {
-  //       method: "POST",
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify(formState)
-  //     });
+  let isSubmitting = $state(false);
+  let error = $state("");
+  let fieldErrors = $state<Record<string, string>>({});
 
-  //     if(!res.ok) throw new Error("Erreur lors de la création");
+  const handleSubmit = async(e:Event) => {
+    e.preventDefault();
 
-  //     const createdTask = await res.json();
-  //     console.log("Nouvelle tâche crée !", createdTask);
+    fieldErrors = {};
+    error = "";
+    isSubmitting = true;
 
-  //     // on vide le formulaire
-  //     formState.title = "";
+    const validate = schema.safeParse(formState);
+    if(!validate.success){
+      const errors = validate.error.flatten().fieldErrors;
+      for(const [field, messages] of Object.entries(errors)){
+        if(messages && messages.length > 0){
+          fieldErrors[field] = messages[0];
+        }
+      }
+      isSubmitting = false;
+      formState.password = "";
+      return;
+    }
 
-  //     // on recharge la liste 
-  //     loadTasks();
-  //   }catch(err){
-  //     console.error(err);
-  //   }finally{
-  //     isSubmitting = false;
-  //   }
-  // }
+    const payload = validate.data;
+
+    try{
+      const { data, error: authError } = await authClient.signIn.email(payload);
+      console.log(data);
+      console.log(authError);
+
+      if(authError){
+        error = authError.message || "Erreur de login";
+        return;
+      }
+
+      goto("/tasks");
+    }catch(err){
+      console.error(err);
+      error = err instanceof Error ? err.message : "erreur inattendue";
+    }finally{
+      isSubmitting = false;
+    }
+  }
+
+  const resetForm = () => {
+    formState.email = "";
+    formState.password = "";
+  }
 </script>
 
-<div>
-  <h1>Auth</h1>
+<div class="main_container">
+  <h1 class="title__page">Connection</h1>
 
-  <form action="">
+  <form action="" onsubmit={(e) => handleSubmit(e)} class="form debug">
+    <div class="input__field">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" bind:value={formState.email}>
+      {#if fieldErrors.email}
+        <span class="error">{fieldErrors.email}</span>
+      {/if}
+    </div>
+
+
 
     <div class="input__field">
-      <label for="username">Username</label>
-      <input type="text" name="username" id="username">
+      <label for="password">Mot de passe</label>
+      <input type="password" name="password" id="password" bind:value={formState.password}>
+      {#if fieldErrors.password}
+        <span class="error">{fieldErrors.password}</span>
+      {/if}
     </div>
 
-    <div class="input__field">
-      <label for="password">Password</label>
-      <input type="password" name="password" id="password">
+    {#if error}
+      <p class="error">{error}</p>
+    {/if}
+      
+    <div class="form__link">
+      <a href="/register" class="link__secondary">Je n'ai pas de compte</a>
     </div>
 
-    <div class="">
-      <a href="/register">Je n'ai pas de compte</a>
-    </div>
+    <div class="form__action">
+      <button type="button" class="btn" onclick={resetForm}>Effacer</button>
 
-    <div class="">
-      <button type="submit" class="btn">Connection</button>
+      <button type="submit" class="btn" disabled={isSubmitting}>{ isSubmitting ? "Connection ..." : "Connexion"}</button>
     </div>
 
   </form>
 </div>
 
 <style>
+  .error {
+    color: red;
+    font-style: italic;
+  }
+
+  .form {
+    width: 50rem;
+    margin: 0 auto;
+  }
+
+  .input__field {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .main_container {
+    padding: 2rem 0;
+  }
+
+  .form__link {
+    font-style: italic;
+    font-size: 1.4rem;
+    text-align: center;
+    margin-top: 1.5rem;
+  }
+
+  .link__secondary {
+    &:hover{
+      color: blue;
+    }
+  }
 </style>
